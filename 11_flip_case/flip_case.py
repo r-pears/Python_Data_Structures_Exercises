280df7d1a135ad6fa4c1ece11a07cfb7e80fb530
Ryjen Pears <ryjen.pears@gmail.com>
2021-01-30 23:26:23 +0100

    python data structure exercises

---
+++ 11_flip_case/flip_case.py
@@ -0,0 +1,18 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    to_swap = to_swap.lower()
+
+    new_phrase = [(char.swapcase() if char.lower() == to_swap else char) for char in phrase]
+
+    return "".join(new_phrase)
